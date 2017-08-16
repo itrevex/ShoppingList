279a4a09da1fa46f70435a9224042d4c625e3778
=Isaac <=trevex2000@live.com>
2017-08-16 10:16:50 +0300

    create login page

---
+++ Scripts/login.py
@@ -0,0 +1,17 @@
+#login.py
+"""
+Login class for housing login methods
+"""
+
+class Login():
+    """
+    Shopping List login class
+    Created on 16/08/2017 by Isaac Ssekamatte
+    """
+    def __init__(self,email,password):
+        self.email = email
+        self.password = password
+        self.online_data = 'steve@steve.com','qwerty'
+    
+        
+        
