58e2250f50cc4064e7c2cabc2398786391d84b87
=Isaac <=trevex2000@live.com>
2017-08-16 10:37:31 +0300

    add check email and check password methods to login

--- Scripts/login.py
+++ Scripts/login.py
@@ -13,7 +13,7 @@ class InValidPasswordExcpetion(Exception):
     password exception class
     """
     pass
-    
+
 class LogIn():
     """
     Shopping List login class
@@ -25,4 +25,14 @@ class LogIn():
         self.online_data = 'steve@steve.com','qwerty'
     
     
-        
+    def check_email(self):
+        """
+        Checks if email matches any emails from server
+        """
+        return self.email == self.online_data[0]
+
+    def check_password(self):
+        """
+        checks if password matches passwords on
+        """
+        return self.password == self.online_data[1]
